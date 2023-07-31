50293568efaa72156fee0d428a7c91fe0d905ba2
Balu7678 <balakrishnagorantla.marolix@gmail.com>
2023-07-31 12:30:17 +0530

    example

---
+++ q8.py
@@ -0,0 +1,12 @@
+#for even_numbers in range(4,15,2):
+#	print(even_numbers,end=' ')
+
+#for odd_numbers in range(1,22,2):
+#	print(odd_numbers,end=' ')
+
+
+num = 83
+if (num % 2) == 0:
+    print("even")
+else:
+    print("odd")
