e553237cf600eeee6e5e170007abb29de1fcfe9c
Balu7678 <balakrishnagorantla.marolix@gmail.com>
2023-07-31 12:01:55 +0530

    q6

---
+++ q6.py
@@ -0,0 +1,16 @@
+#age = int(input("Enter Age : "))
+#if age>=18:
+#       status="Eligible"
+#else:
+#    status="Not Eligible"
+
+#print("You are ",status," for Vote.")
+
+
+#age = 13
+#if age>=18:
+#    print("Eligible")
+#else:
+#    print("Not Eligible")
+
+
