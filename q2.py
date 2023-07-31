07858b4fb702b14ff03dbb0f7114edf9303ebc0f
Balu7678 <balakrishnagorantla.marolix@gmail.com>
2023-07-31 10:49:58 +0530

    second q2

---
+++ q2.py
@@ -0,0 +1,16 @@
+num1, num2 = 20 , 30
+if num1>num2:
+  print(num1)
+else:
+  print(num2)
+
+#num1, num2 = 20 , 30
+#print((num1 if num1>num2 else num2))
+
+
+#num1 = int(input("Enter first number: "))      
+#num2 = int(input("Enter second number: ")) 
+#if num1 >= num2:   
+ #   print("Fisrt number is greater than the second number.",num1)
+#else:   
+ #   print("Second number is greater than the First number.",num2) 
