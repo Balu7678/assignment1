fd9daa5d48aae47c8de96ac06aca71a4bc5e83b1
Balu7678 <balakrishnagorantla.marolix@gmail.com>
2023-07-31 11:32:29 +0530

    q4

---
+++ q4.py
@@ -0,0 +1,15 @@
+string = "PythonPool"
+# number of words
+n = len(string)
+print(n)
+ 
+if n%2 == 0:
+  string1 = string[0:n//2]
+  print("First Half of String:",string1)
+  
+else:
+  string1 = string[0:(n//2+1)]
+  print("First Half of String:",string1)
+  
+
+
