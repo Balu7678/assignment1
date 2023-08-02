047e561188055e75e2ebac929b9f1eed5c73a3e5
Balu7678 <balakrishnagorantla.marolix@gmail.com>
2023-08-02 15:28:41 +0530

    string

--- Strings.py
+++ Strings.py
@@ -6,3 +6,20 @@ else:
     print("both strings are not same")
 
 
+s=str(input())
+print(len(s))
+
+
+s3= "python programing"
+print(s3[10])
+
+print(s[-7:7:1])
+
+
+print(s+s3)
+
+
+print(s3*6)
+
+
+#for i in s[::-1] reverse printing
