bffd3b12ecb48e907e524a84a22173a0b1527890
Balu7678 <balakrishnagorantla.marolix@gmail.com>
2023-08-02 15:26:40 +0530

    string

---
+++ Strings.py
@@ -0,0 +1,8 @@
+s1=str(input("enter string 1 : "))
+s2=str(input("enter string 2 : "))
+if s1==s2:
+    print("both strings are same") #using boolean true or false also.
+else:
+    print("both strings are not same")
+
+
